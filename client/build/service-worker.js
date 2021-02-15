4c7f9b1463a300ca9317b5e2452be75d4e971d09
Giovanni Prisco <gprisco01@gmail.com>
2021-02-15 13:08:29 +0100

    New build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0b801ccfc92a6007883d262e2171066.js"
+  "/precache-manifest.6bafe153a6c10fa813f8bb5a352b56a9.js"
 );
 
 self.addEventListener('message', (event) => {
